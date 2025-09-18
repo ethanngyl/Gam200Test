946eacd92502ed02f6520884fbbabf49f960df13
Wei Liang <weileong.tan@digipen.edu>
2025-09-18 14:25:55 +0800

    1st push
    Saving files

--- engine/Graphics/Shader.cpp
+++ engine/Graphics/Shader.cpp
@@ -1,1 +1,107 @@
 #include "Graphics/Shader.h"
+#include <GL/glew.h>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <unordered_map>
+
+Shader::Shader(const std::string& vertexPath, const std::string& fragmentPath)
+{
+    std::string vertexSrc = LoadFile(vertexPath);
+    std::string fragmentSrc = LoadFile(fragmentPath);
+    m_RendererID = CreateProgram(vertexSrc, fragmentSrc);
+}
+
+void Shader::Bind() const
+{
+    glUseProgram(m_RendererID);
+}
+
+void Shader::Unbind() const
+{
+    glUseProgram(0);
+}
+
+std::string Shader::LoadFile(const std::string& path)
+{
+    std::ifstream file(path);
+    std::stringstream buffer;
+    buffer << file.rdbuf();
+    return buffer.str();
+}
+
+unsigned int Shader::CompileShader(unsigned int type, const std::string& source)
+{
+    unsigned int id = glCreateShader(type);
+    const char* src = source.c_str();
+    glShaderSource(id, 1, &src, nullptr);
+    glCompileShader(id);
+
+    int success;
+    glGetShaderiv(id, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        char infoLog[512];
+        glGetShaderInfoLog(id, 512, nullptr, infoLog);
+        std::cerr << "Shader compilation failed: " << infoLog << std::endl;
+    }
+    return id;
+}
+
+unsigned int Shader::CreateProgram(const std::string& vertexSrc, const std::string& fragmentSrc)
+{
+    unsigned int program = glCreateProgram();
+    unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexSrc);
+    unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentSrc);
+
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+    glLinkProgram(program);
+
+    int success;
+    glGetProgramiv(program, GL_LINK_STATUS, &success);
+    if (!success)
+    {
+        char infoLog[512];
+        glGetProgramInfoLog(program, 512, nullptr, infoLog);
+        std::cerr << "Program linking failed: " << infoLog << std::endl;
+    }
+
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
+    return program;
+}
+
+int Shader::GetUniformLocation(const std::string& name)
+{
+    if (m_UniformLocationCache.find(name) != m_UniformLocationCache.end())
+        return m_UniformLocationCache[name];
+
+    int location = glGetUniformLocation(m_RendererID, name.c_str());
+    if (location == -1)
+        std::cerr << "Warning: uniform '" << name << "' doesn't exist!" << std::endl;
+
+    m_UniformLocationCache[name] = location;
+    return location;
+}
+
+void Shader::SetUniform1i(const std::string& name, int value)
+{
+    glUniform1i(GetUniformLocation(name), value);
+}
+
+void Shader::SetUniform1f(const std::string& name, float value)
+{
+    glUniform1f(GetUniformLocation(name), value);
+}
+
+void Shader::SetUniformVec3(const std::string& name, const glm::vec3& value)
+{
+    glUniform3fv(GetUniformLocation(name), 1, &value[0]);
+}
+
+void Shader::SetUniformMat4(const std::string& name, const glm::mat4& matrix)
+{
+    glUniformMatrix4fv(GetUniformLocation(name), 1, GL_FALSE, &matrix[0][0]);
+}
