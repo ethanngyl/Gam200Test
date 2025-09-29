14aad18c02a2c6f036a2746911eb771d9026764d
SekyoT <carl4523@gmail.com>
2025-09-29 15:22:47 +0800

    Reapply "Added my Textsys to main.cpp"
    This reverts commit c20c2955f4185d34e0b6d69b9b872000bfd4eb51.

--- engine/main.cpp
+++ engine/main.cpp
@@ -51,6 +51,9 @@ int WINAPI WinMain(    _In_ HINSTANCE hInstance,
     engine.AddSystem(inputSys);
     engine.AddSystem(collisionSys);
     engine.AddSystem(mathSys);
+	engine.AddSystem(textSys);
+
+
 
     std::cout << "Systems added. Initializing engine...\n";
 
