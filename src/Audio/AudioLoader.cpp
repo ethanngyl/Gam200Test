b23e8c3749810138ef03f7478551e8b44fb1816d
GYQ-SIT <2301516@sit.singaporetech.edu.sg>
2025-11-28 23:49:15 +0800

    fix text not being draw when using ALT_TAB

--- src/Audio/AudioLoader.cpp
+++ src/Audio/AudioLoader.cpp
@@ -150,8 +150,7 @@ namespace Framework {
 
     bool AudioLoader::SetMasterVolume(float volume, const std::string& filepath) {
         // Clamp volume to valid range
-        settings.masterVolume = std::max(0.0f, std::min(1.0f, volume));
-
+        settings.masterVolume = (std::max)(0.0f, (std::min)(1.0f, volume));
         // Save to JSON file
         return SaveAudioConfig(filepath);
     }
