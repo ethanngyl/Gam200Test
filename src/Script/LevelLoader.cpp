23202f1629e552d62f9ae40854cfdfda0cef31d4
ethanngyl <ngylethan@gmail.com>
2026-03-11 17:18:14 +0800

    e

--- src/Script/LevelLoader.cpp
+++ src/Script/LevelLoader.cpp
@@ -768,6 +768,7 @@ namespace Framework {
         lua_register(L, "CreateParticleEffect", Lua_CreateParticleEffect);
         lua_register(L, "SetActivePlayerIndex", Lua_SetActivePlayerIndex);
 
+        lua_register(L, "SpawnParticleEmitterEthan", Lua_SpawnParticleEmitterEthan);
         LOG_INFO("LevelLoader", "API registered");
     }
 
